c47b619bfcd7437835a39e6827cc25d358f33c52
xaridar <xaridara@gmail.com>
2022-09-28 02:35:02 -0400

    redeploy

--- deploy.js
+++ deploy.js
@@ -9,21 +9,21 @@ pages.renderFiles([
     {
         view: 'index.ejs',
         url: '',
-        options: { header: undefined, pages: ['about, projects, resume'] },
+        options: { header: undefined, pages: ['about', 'projects', 'resume'] },
     },
     {
         view: 'about.ejs',
         url: '/about',
-        options: { header: 'about', pages: ['about, projects, resume'] },
+        options: { header: 'about', pages: ['about', 'projects', 'resume'] },
     },
     {
         view: 'projects.ejs',
         url: '/projects',
-        options: { header: 'projects', pages: ['about, projects, resume'] },
+        options: { header: 'projects', pages: ['about', 'projects', 'resume'] },
     },
     {
         view: 'resume.ejs',
         url: '/resume',
-        options: { header: 'resume', pages: ['about, projects, resume'] },
+        options: { header: 'resume', pages: ['about', 'projects', 'resume'] },
     },
 ]);
